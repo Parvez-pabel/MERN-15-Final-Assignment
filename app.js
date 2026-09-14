60cb641a2004e0e71cc33013a9789a723e30d78d
Parvez-pabel <pabelalomparvez2233@gmail.com>
2026-09-14 17:00:45 +0600

    device moved

--- app.js
+++ app.js
@@ -1,31 +1,23 @@
-import 'dotenv/config';
+import "dotenv/config";
 import express from "express";
-import ConnectDB from "../server/src/utility/db.js";
-import userRouter from './src/routes/api.js';
-import cookieParser from 'cookie-parser';
-
-
+import cookieParser from "cookie-parser";
+import ConnectDB from "./src/utility/db.js";
+import userRouter from "./src/routes/api.js";
 
 const app = express();
 
 //db connection
-
 
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
 ConnectDB();
 //routes
 app.use("/api/v1", userRouter);
 app.get("/", (req, res) => {
-    res.json({ message: 'API is running successfully!' });
+  res.json({ message: "API is running successfully!" });
 });
-
-
-
-
 
 export default app;
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-
-import app from './app.js';
-
+import "dotenv/config";
+import app from "./app.js";
 
 const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+  console.log(`Server is running on port ${PORT}`);
+});
