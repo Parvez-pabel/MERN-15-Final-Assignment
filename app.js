c5a131a73b0b126796f90a8c9c106399a03fab91
Parvez-pabel <pabelalomparvez2233@gmail.com>
2026-09-15 17:46:57 +0600

    client link added

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ dotenv.config();
 const app = express();
 
 //middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173", 
+    credentials: true, 
+  }),
+);
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
